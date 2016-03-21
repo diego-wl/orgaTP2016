4b214aae87a6ebf97375203b6a133b319402010d
diego.serra <diego.serra@gmail.com>
2016-03-21 00:15:47 -0300

    creacion matrix.h y matrix.c

---
+++ Tp0/Source/matrix.c
@@ -0,0 +1,46 @@
+#include "matrix.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Constructor de matrix_t
+matrix_t* create_matrix(size_t rows, size_t cols){
+	matrix_t* matrix = (matrix_t*)malloc(sizeof(matrix_t));
+	matrix->rows = rows;
+	matrix->cols = cols;
+	matrix->array = (double*)malloc(sizeof(rows)*sizeof(cols));
+	return matrix;
+}
+
+// Destructor de matrix_t
+void destroy_matrix(matrix_t* m){
+
+}
+
+// Imprime matrix_t sobre el file pointer fp en el formato solicitado
+// por el enunciado
+int print_matrix(FILE* fp, matrix_t* m){
+	return 0;
+}
+
+// Multiplica las matrices en m1 y m2
+matrix_t* matrix_multiply(matrix_t* m1, matrix_t* m2){
+	int m1_index = 0;
+	int m2_index = 0;
+	int m2_aux = 0;
+	int index = 0;
+	matrix_t* result = create_matrix(m1->rows,m1->cols);
+
+	for( m1_index = 0; m1_index < m1->cols * m1->rows; ){
+		for( m2_aux = 0; m2_aux < m2->rows; ){
+			result->array[index] += m1->array[m1_index] * m2->array[m2_index];
+			m2_aux++;
+			m1_index++;
+			m2_index += m2->rows;
+		}
+		index++;
+		m1_index = index / m1->cols;
+		m2_aux = 0;
+	}
+	return result;
+}
