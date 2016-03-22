013a8fbe92ca23679f370b22a9a38dc5539e2dcc
diego.serra <diego.serra@gmail.com>
2016-03-22 02:00:42 -0300

    integracion

--- Tp0/Source/Tp0.c
+++ Tp0/Source/Tp0.c
@@ -11,7 +11,60 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "matrix.h"
+
+typedef struct matrix {
+	size_t rows;
+	size_t cols;
+	double* array;
+} matrix_t;
+
+// Constructor de matrix_t
+matrix_t* create_matrix(size_t rows, size_t cols){
+	matrix_t* matrix = (matrix_t*)malloc(sizeof(matrix_t));
+	matrix->rows = rows;
+	matrix->cols = cols;
+	matrix->array = (double*)malloc(sizeof(rows)*sizeof(cols));
+	return matrix;
+}
+
+// Destructor de matrix_t
+void destroy_matrix(matrix_t* m){
+
+}
+
+// Imprime matrix_t sobre el file pointer fp en el formato solicitado
+// por el enunciado
+int print_matrix(FILE* fp, matrix_t* m){
+	int i = 0;
+	for(i; i<(m->cols)*(m->cols); i++){
+		printf("index: %d, value: %f \n",i,m->array[i]);
+	}
+
+	return 0;
+}
+
+// Multiplica las matrices en m1 y m2
+matrix_t* matrix_multiply(matrix_t* m1, matrix_t* m2){
+	int m1_index = 0;
+	int m2_index = 0;
+	int m2_aux = 0;
+	int index = 0;
+	matrix_t* result = create_matrix(m1->rows,m1->cols);
+
+	for( m1_index = 0; m1_index <= m1->rows * m1->cols; ){
+		m1_index = (index / m1->cols) * m1->rows;
+		for( m2_aux = 0; m2_aux < m2->rows; ){
+			result->array[index] += m1->array[m1_index] * m2->array[m2_index];
+			m2_aux++;
+			m1_index++;
+			m2_index += m2->rows;
+		}
+		index++;
+		m2_index = index % m2->cols;
+		m2_aux = 0;
+	}
+	return result;
+}
 
 void show_help(){
 	printf("Usage:\n");
