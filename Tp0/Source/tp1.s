ddb96b3ba5df289f740f5180ce8cecb594330c1b
mauruch <capolupo.mauro@gmail.com>
2016-04-28 17:37:53 -0300

    va cobrando forma

--- Tp0/Source/tp1.s
+++ Tp0/Source/tp1.s
@@ -31,6 +31,8 @@
 .text
 .globl matrix_multiply
 
+matrix_multiply:
+
 #bloque de codigo para PIC
 .set noreorder
 .cpload t9
@@ -43,27 +45,42 @@ subu sp, sp, 40  //alcanza ?
 .cprestore 24 //sw gp, sp(24) / lw gp, 24(sp).
 
 sw $fp, 28(sp)
-sw $ra, 32(sp)
+sw ra, 32(sp)
 
 move $fp, sp //uso fp en vez de sp.
 
-#salvo los args
+#salvo los args fuera del stack frame.
 sw a0, 40($fp)
 sw a1, 44($fp)
 
+sw zero, 0($fp)  //En 0(sp) tengo int m1_index = 0
+sw zero, 4($fp)  //En 4(sp) tengo int m2_index = 0
+sw zero, 8($fp)  //En 8(sp) tengo int m2_aux = 0
+sw zero, 12($fp) //En 12(sp) tengo int index = 0
+
+//en a0 tengo m1 (0-4->row, 4-8 ->cols)
+add a1, a0, 4 //a1 = cols
+la t9, create_matrix
+jal ra, t9
+
+sw v0, 14($fp) //guardo el retorno de create_matrix
+
+for:
+//for (m1_index = 0; m1_index <= m1->rows * m1->cols;)
+lw t0, 0($fp) //m1_index
+mulo t1, a0, a1 // rows * cols
+
+bgt t0, t1, exit_for //si m1_index es mayor que (rows * cols) finalizo el loop
 
 
 
-
-
-
-
-
-
-
-
-
-
+exit_for:
+//destruyo el stack frame
+lw gp, 24(sp)
+lw fp, 28(sp)
+addu sp, sp, 40
+lw ra, 32(sp)
+j ra
 
 
 
